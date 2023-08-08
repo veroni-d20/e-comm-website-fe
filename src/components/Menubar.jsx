addcacedbf1ad8e6c2fa50844b2d2d1ccadd8d7d
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-08-08 12:06:27 +0530

    navabr component

---
+++ src/components/Menubar.jsx
@@ -0,0 +1,66 @@
+import { Nav, Navbar, Container, Offcanvas } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/logo.png";
+
+export default function Menubar() {
+  return (
+    <Navbar
+      key="md"
+      expand="md"
+      fixed="top"
+      bg="light"
+      data-bs-theme="light"
+      style={{ height: "80px", zIndex: 9 }}
+      className="bg-body-tertiary mb-3"
+    >
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img
+            src={logo}
+            width="100"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Ecom"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-$md`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-$md`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-$md`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-$md`}>
+              Wyntra
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="me-auto">
+              <Nav.Link className="navlink" href="#home">
+                MEN
+              </Nav.Link>
+              <Nav.Link className="navlink" href="#features">
+                WOMEN
+              </Nav.Link>
+              <Nav.Link className="navlink" href="#pricing">
+                KIDS
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link className="navlink-icon" href="#pricing">
+                <FontAwesomeIcon icon={faHeart} size="lg" className="mx-2" />
+                <p style={{ fontSize: "11px" }}>Wishlist</p>
+              </Nav.Link>
+              <Nav.Link className="navlink-icon" href="#pricing">
+                <FontAwesomeIcon icon={faCartShopping} size="lg" />
+                <p style={{ fontSize: "11px" }}>Cart</p>
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,82 +1,11 @@
-import {
-  Button,
-  Card,
-  Form,
-  FloatingLabel,
-  Nav,
-  Navbar,
-  Container,
-  Offcanvas,
-} from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import logo from "../assets/logo.png";
+import { Button, Card, Form, FloatingLabel } from "react-bootstrap";
+import Menubar from "../components/Menubar";
 
 export default function Login() {
   return (
     <>
       <div className="vh-100" style={{ background: "#f1e1de" }}>
-        <Navbar
-          key="md"
-          expand="md"
-          fixed="top"
-          bg="light"
-          data-bs-theme="light"
-          style={{ height: "80px", zIndex: 9 }}
-          className="bg-body-tertiary mb-3"
-        >
-          <Container fluid>
-            <Navbar.Brand href="#">
-              <img
-                src={logo}
-                width="100"
-                height="50"
-                className="d-inline-block align-top"
-                alt="Ecom"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-$md`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-$md`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-$md`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-$md`}>
-                  Wyntra
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="me-auto">
-                  <Nav.Link className="navlink" href="#home">
-                    MEN
-                  </Nav.Link>
-                  <Nav.Link className="navlink" href="#features">
-                    WOMEN
-                  </Nav.Link>
-                  <Nav.Link className="navlink" href="#pricing">
-                    KIDS
-                  </Nav.Link>
-                </Nav>
-                <Nav>
-                  <Nav.Link className="navlink-icon" href="#pricing">
-                    <FontAwesomeIcon
-                      icon={faHeart}
-                      size="lg"
-                      className="mx-2"
-                    />
-                    <p style={{ fontSize: "11px" }}>Wishlist</p>
-                  </Nav.Link>
-                  <Nav.Link className="navlink-icon" href="#pricing">
-                    <FontAwesomeIcon icon={faCartShopping} size="lg" />
-                    <p style={{ fontSize: "11px" }}>Cart</p>
-                  </Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+        <Menubar />
         <div className="vh-100 vw-100 m-0 d-flex align-items-center justify-content-center">
           <Card style={{ width: "23rem" }} className="shadow login-card">
             <Card.Img
@@ -103,6 +32,14 @@ export default function Login() {
                 >
                   CONTINUE
                 </Button>
+                <div className="mt-3 text-center">
+                  <p>
+                    Don't have a account?{" "}
+                    <a href="/register" style={{ color: "black" }}>
+                      Sign up
+                    </a>
+                  </p>
+                </div>
               </div>
             </Card.Body>
           </Card>
